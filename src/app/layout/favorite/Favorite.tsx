1c2f7f0924fbee65fae4d40226313cbe4bc4eeb6
NgocMinh10122001 <phamngocminh10122001@gmail.com>
2024-01-23 19:52:48 +0700

    create page and feature favorite movie

---
+++ src/app/layout/favorite/Favorite.tsx
@@ -0,0 +1,29 @@
+"use client";
+import { deleteAllFavoriteMovie } from "@/server-action/user";
+import React, { memo } from "react";
+
+interface IProps {
+  id: string;
+}
+function Favorite(props: IProps) {
+  let { id } = props;
+  const handleDeleteAllFavoriteMovie = () => {
+    deleteAllFavoriteMovie(id);
+  };
+  return (
+    <div className="flex justify-between items-center mb-8">
+      <p className="uppercase font-bold text-black dark:text-white text-2xl">
+        PHIM ĐÃ LƯU
+      </p>
+      <button
+        typeof="button"
+        className="w-fit py-2 px-4 uppercase font-light text-white   text-xs bg-red-400 hover:bg-red-500   cursor-pointer duration-200 ease-in-out rounded-sm"
+        onClick={handleDeleteAllFavoriteMovie}
+      >
+        xoá tất cả
+      </button>
+    </div>
+  );
+}
+
+export default memo(Favorite);
