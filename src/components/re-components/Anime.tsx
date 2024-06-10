65e621843ed9742b5c72d9c716ce152d82173df0
Minhdz <120233971+NgocMinh10122001@users.noreply.github.com>
2024-06-10 13:32:17 +0700

    Update Anime.tsx

--- src/components/re-components/Anime.tsx
+++ src/components/re-components/Anime.tsx
@@ -26,7 +26,7 @@ function Anime(props: IAnimes) {
   };
   useResizeAnimeElements();
   return (
-    <div className="w-full grid grid-cols-2 gap-2 sm:gap-2 lg:gap-4 xl:gap-4 2xl:gap-2 sm:grid-cols-3 md:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6   min-[1921px]:gap-2  font-sans">
+    <div className="w-full grid grid-cols-2 gap-2 sm:gap-2 lg:gap-4 xl:gap-4  sm:grid-cols-3 md:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6     font-sans">
       {animes?.length > 0 &&
         animes.map((item) => {
           return (
